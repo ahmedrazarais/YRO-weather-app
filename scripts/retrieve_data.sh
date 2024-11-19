8d757f0a9524a84855eac329f4d65abd50fc9f3b
ahmedrazarais <razarais28@gmail.com>
2024-11-19 19:14:37 +0500

    script file for automate run

--- scripts/retrieve_data.sh
+++ scripts/retrieve_data.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Navigate to the project root directory
-cd "/mnt/c/Users/ideal pc/Desktop/cew"
+cd "/mnt/c/Users/ideal pc/Desktop/YRO-weather-app/"
 
-# Compile the C program
-gcc src/main.c src/weather.c -o weather -lcurl -lcjson
+# Compile the C program (ensure this path is correct for your environment)
+gcc src/main.c src/weather.c -o weather_app -lcurl -lcjson
 
-# Run the program
+# Run the program (ensure it's compiled correctly)
 ./weather_app
-
